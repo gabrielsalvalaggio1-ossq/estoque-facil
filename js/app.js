f31b14a8bb693180976102021558ede0eb1452db
Gabriel Salvalaggio dos Santos <gabrielsalvalaggio@mprs.mp.br>
2026-07-07 21:23:18 -0300

    ajustes largura

--- js/app.js
+++ js/app.js
@@ -1716,10 +1716,10 @@ function abrirMenuExportar() {
 const CHAVE_ONBOARDING = 'estoqueFacilOnboardingVisto';
 
 const PRODUTOS_EXEMPLO = [
-  { nome: 'Coca-Cola lata', preco: 6, estoque: 24, estoqueMinimo: 6, categoria: 'Bebidas', imagem:'img/agua500ml.png'},
-  { nome: 'Salgado assado', preco: 7, estoque: 15, estoqueMinimo: 5, categoria: 'Salgados' },
-  { nome: 'Água mineral 500ml', preco: 3, estoque: 30, estoqueMinimo: 8, categoria: 'Bebidas' },
-  { nome: 'Brigadeiro', preco: 2.5, estoque: 20, estoqueMinimo: 5, categoria: 'Doces' }
+  { nome: 'Coca-Cola lata', preco: 6, estoque: 24, estoqueMinimo: 6, categoria: 'Bebidas', imagem:'img/cocacola.jpg'},
+  { nome: 'Salgado assado', preco: 7, estoque: 15, estoqueMinimo: 5, categoria: 'Salgados', imagem:'img/salgado.jpg' },
+  { nome: 'Água mineral 500ml', preco: 3, estoque: 30, estoqueMinimo: 8, categoria: 'Bebidas',imagem:'img/agua500ml.png' },
+  { nome: 'Brigadeiro', preco: 2.5, estoque: 20, estoqueMinimo: 5, categoria: 'Doces',imagem:'img/brigadeiro.png' }
 ];
 
 function abrirOnboarding() {
